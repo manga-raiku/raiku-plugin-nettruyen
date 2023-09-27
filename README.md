9eed411c861da1f12b848c91715762d8419d9677
Tachibana Shin <tachibshin@duck.com>
2023-09-27 07:55:23 +0700

    add build readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Plugin for Raiku app
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,14 @@
-import { existsSync, rmdirSync } from "fs"
+// eslint-disable-next-line n/no-unsupported-features/node-builtins
+import { copyFileSync, existsSync, mkdirSync, rmdirSync } from "fs"
 import { join } from "path"
 
 import { build } from "vite"
 
 if (existsSync(join(__dirname, "../dist")))
   rmdirSync(join(__dirname, "../dist"), { recursive: true })
+
+mkdirSync(join(__dirname, "../dist"))
+copyFileSync(join(__dirname, "../README.md"), join(__dirname, "../dist/README.md"))
 
 build({
   base: "./",
