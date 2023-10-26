b7483aaececc7024237ceae7e1a796f9b50ea970
Tachibana Shin <tachibshin@duck.com>
2023-10-26 03:42:16 +0000

    fix test

--- test/setup-file.ts
+++ test/setup-file.ts
@@ -1,4 +1,11 @@
 import { parseDom } from "raiku-pgs/thread"
+import { AppInfo } from "raiku-pgs/plugin"
+
+const AppInfo: AppInfo = {
+  mode: "spa",
+  extension: false,
+  version: "0.0.1"
+}
 
 // eslint-disable-next-line functional/immutable-data
-Object.assign(self, { parseDom })
+Object.assign(self, { parseDom, setReferrers: () => { }, AppInfo })
