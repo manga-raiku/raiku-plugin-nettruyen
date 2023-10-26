797073d3367cf60e32a953b8ee7add7ea50925ef
Tachibana Shin <tachibshin@duck.com>
2023-10-26 03:50:07 +0000

    lint

--- test/setup-file.ts
+++ test/setup-file.ts
@@ -1,11 +1,12 @@
-import { parseDom } from "raiku-pgs/thread"
 import { AppInfo } from "raiku-pgs/plugin"
+import { parseDom } from "raiku-pgs/thread"
 
+// eslint-disable-next-line no-redeclare, no-import-assign
 const AppInfo: AppInfo = {
   mode: "spa",
   extension: false,
   version: "0.0.1"
 }
 
-// eslint-disable-next-line functional/immutable-data
+// eslint-disable-next-line functional/immutable-data, @typescript-eslint/no-empty-function
 Object.assign(self, { parseDom, setReferrers: () => { }, AppInfo })
