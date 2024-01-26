8f1a07a484c9e02efcc4990717f3c84f01a44f5c
Tachibana Shin <tachibshin@duck.com>
2024-01-26 12:20:52 +0700

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
-export const CURL = "https://www.nettruyenclub.com/"
-export const API_CURL = "https://f.nettruyenclub.com/"
+export const CURL = "https://www.nettruyenus.net/"
+export const API_CURL = "https://f.nettruyenus.net/"
